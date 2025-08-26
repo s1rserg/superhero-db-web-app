519bdf8e4caf7761613988e4c3b68c30937b3241
s1rserg <sirserg05@gmail.com>
2025-08-26 19:58:43 +0300

    feat: update configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,11 @@
 import js from '@eslint/js';
 import ts from 'typescript-eslint';
-import prettier from 'eslint-plugin-prettier';
 
 export default [
   js.configs.recommended,
   ...ts.configs.recommended,
   {
-    plugins: {
-      prettier,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-    },
+    rules: {},
   },
   {
     ignores: ['dist', 'node_modules'],
